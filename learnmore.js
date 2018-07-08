a61ed450bc31e4ee2a01c8d14becae1cd51825ee
EnMin0206 <sohenmin62@hotmail.co.uk>
2018-07-09 02:31:54 +0800

    javascript

---
+++ learnmore.js
@@ -0,0 +1,93 @@
+jQuery(window).load(function(){
+  $(document).ready(function(){
+      $('.more').click(function(){
+	     $('.details').fadeIn(1000);
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.more1').click(function(){
+	     $('.details1').fadeIn(1000);
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.more2').click(function(){
+	     $('.details2').fadeIn(1000);
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.more3').click(function(){
+	     $('.details3').fadeIn(1000);
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.more4').click(function(){
+	     $('.details4').fadeIn(1000);
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.more5').click(function(){
+	     $('.details5').fadeIn(1000);
+		  return false;
+	  });  
+  });
+   $(document).ready(function(){
+      $('.less').click(function(){
+	     $('.details').hide();
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.less1').click(function(){
+	     $('.details1').hide();
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.less2').click(function(){
+	     $('.details2').hide();
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.less3').click(function(){
+	     $('.details3').hide();
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.less4').click(function(){
+	     $('.details4').hide();
+		  return false;
+	  });  
+  });
+    $(document).ready(function(){
+      $('.less5').click(function(){
+	     $('.details5').hide();
+		  return false;
+		
+	  });  
+  });
+  function scrollWin1() {
+    window.scrollTo(0, 325);
+}
+function scrollWin2() {
+    window.scrollTo(0, 750);
+}
+function scrollWin3() {
+    window.scrollTo(0, 325);
+}
+function scrollWin4() {
+    window.scrollTo(0, 750);
+}
+function scrollWin5() {
+    window.scrollTo(0, 325);
+}
+function scrollWin6() {
+    window.scrollTo(0, 750);
+}
+}); 
